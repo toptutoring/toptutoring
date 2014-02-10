4c7020aeee0a4363edd9dee92a01d31e16b1868d
tadp <june@tutormentor.org>
2014-02-09 19:43:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toptutoring::Application.config.secret_key_base = '5199fd6b55684e747a8e802a7fa86a33be5a07fa7249286579fedb7cd72cf8c4bc0bfb226f1441b72ff3f0a4ab0a63dfdf591d3170c9bcdbc2952ce4bdcaa3ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toptutoring::Application.config.session_store :cookie_store, key: '_toptutoring_session'
