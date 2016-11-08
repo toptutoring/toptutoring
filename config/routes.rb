40f968c22142f72304cf21bd90f4ca1ac283407f
Alexandra Persea <perseaalexandra@gmail.com>
2016-11-08 16:17:06 +0200

    Payment feature implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Toptutoring::Application.routes.draw do
   get "/New_SAT_2016.html" => "pages#New_SAT_2016"
   get "/New_SAT_Writing.html" => "pages#New_Sat_Writing"
   get "/services.html" => "pages#services"
-
+  get "/pay_online" => "payments#show"
+  post "/payments" => "payments#create"
+  get "/confirmation" => "payments#confirmation"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -50,7 +52,7 @@ Toptutoring::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
