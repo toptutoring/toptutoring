a8ebf50c0e5a9c1e99953466f755761ca451f11f
Tad Park <tadp@users.noreply.github.com>
2016-12-19 21:39:17 -0800

    Add basic login, forgot your password and remember me

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < Clearance::SessionsController
+  def new
+  end
+
+  def create
+    set_remember_me
+    super
+  end
+
+  private
+
+  def set_remember_me
+    if params[:session][:remember_me]
+      cookies.permanent[:remember_me] = true
+    else
+      cookies.delete(:remember_me)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root to: "sessions#new"
+  get "/reset_password" => "passwords#new", as: "reset_password"
+  get "/example_dashboard" => "pages#example_dashboard"
 end
