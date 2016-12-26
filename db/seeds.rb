0e1aacc3e942fb993043667e9568b929ff244d80
Alexandra Persea <perseaalexandra@gmail.com>
2016-12-26 20:12:39 +0200

    General feedback

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tutor = User.create!(email: "tutor@gmail.com", password: 'password', name: 'tutor').create_tutor(subject: "Math", activity_type: "VAT")
-parent = User.create(email: "parent@gmail.com", password: 'password', name: 'parent')
-student = parent.create_student(email: "student@gmail.com", name: 'student', subject: "Math", activity_type: "SAT")
+tutor = User.create!(email: "tutor@example.com", password: 'password', name: 'tutor').create_tutor(subject: "Math", activity_type: "VAT")
+parent = User.create(email: "parent@example.com", password: 'password', name: 'parent')
+student = parent.create_student(email: "student@example.com", name: 'student', subject: "Math", activity_type: "SAT")
