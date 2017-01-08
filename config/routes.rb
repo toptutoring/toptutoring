5dbf66289c3891bfbfbb202adfec2be8280d9439
Tad Park <tadp@users.noreply.github.com>
2017-01-08 15:19:50 -0800

    Fix payment route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   post "/passwords" => "passwords#create"
   get "/example_dashboard" => "pages#example_dashboard"
   get "/calendar" => "pages#calendar"
-  get "payment" => "one_time_payments#new"
-  post "payments/one_time" => "one_time_payments#create"
+  get "/payment" => "one_time_payments#new"
+  post "/payments/one_time" => "one_time_payments#create"
   get "/confirmation" => "one_time_payments#confirmation"
 
   # Omniauth routes
