91022dcb136254f4b3a3ced60fceb64b91f5acf0
Alexandra Persea <perseaalexandra@gmail.com>
2017-01-18 13:41:41 +0200

    Signup fix.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115124513) do
+ActiveRecord::Schema.define(version: 20170118113545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
