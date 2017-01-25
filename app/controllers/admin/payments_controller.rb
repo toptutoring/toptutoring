d63f20d1b72c1cebac18258b84800111bed619ef
Alexandra Persea <perseaalexandra@gmail.com>
2017-01-26 01:34:21 +0200

    90% restriction added to director payments.

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -2,6 +2,7 @@ module Admin
   class PaymentsController < ApplicationController
     before_action :require_login
     before_action :set_funding_sources, :set_auth_tutor, only: :new
+    before_action :validate_payment, only: :create
 
     def index
       @payments = Payment.from_parents
@@ -14,9 +15,13 @@ module Admin
     def create
       @payment = Payment.new(payment_params)
       @payment.destination = User.find(@payment.payee_id).auth_uid
-      @payment.save
-      TransferWorker.perform_async(@payment.id)
-      redirect_to admin_payments_path
+      if @payment.save
+        TransferWorker.perform_async(@payment.id)
+        redirect_to admin_payments_path, notice: 'Payment successfully made.'
+      else
+        flash[:danger] = "You must provide your card information before making a payment."
+        redirect_to :back
+      end
     end
 
     private
@@ -31,6 +36,15 @@ module Admin
 
     def set_auth_tutor
       @tutors = User.all.select { |user| user.tutor? && user.has_external_auth? }
+    end
+
+    def validate_payment
+      if current_user.director?
+        if User.find(payment_params[:payee_id]).balance.to_f * 0.9 < payment_params[:amount].to_f
+          flash[:danger] = 'Tutor cannot be payed more than 90% of his earnings.'
+          redirect_to :back
+        end
+      end
     end
   end
 end
