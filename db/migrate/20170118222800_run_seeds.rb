c164724ff212f8d90a8ad4f4623b85760e0e62e8
Alexandra Persea <perseaalexandra@gmail.com>
2017-02-02 01:02:09 +0200

    One time payment fixed.

--- db/migrate/20170118222800_run_seeds.rb
+++ db/migrate/20170118222800_run_seeds.rb
@@ -1,5 +1,6 @@
 class RunSeeds < ActiveRecord::Migration[5.0]
+  # Prod seeds have been updated with new models and attributes.
   def change
-    Rake::Task['prod:seed'].invoke
+    #Rake::Task['prod:seed'].invoke
   end
 end
