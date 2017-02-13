fc5f0be5d84523922a45905a76265cc74a1492fc
Alexandra Persea <perseaalexandra@gmail.com>
2017-02-13 22:31:09 +0200

    Redirect force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  if Rails.env.production?
+    force_ssl(host: ENV['SSL_APPLICATION_HOST'])
+  end
 end
