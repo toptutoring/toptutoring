c3afd81943f0815d3ba237fe785df0a74f815496
Christian Hecker <christianhecker36@yahoo.com>
2017-05-05 15:29:10 -0500

    Move the multistep tutor signup from tutor#update to tutor#new.
    This will keep the database from being populated by a bunch of random
    tutors by making it more difficult to sign up.
    Add a signup cover page which will redirect the tutor to the real signup.
    Move the subject selection param functionality to the tutors controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,8 @@ class UsersController < Clearance::SessionsController
 
   def update
     if current_user.update_attributes(user_params)
-      if !current_user.is_student? && !current_user.is_tutor?
+      if !current_user.is_student?
         current_user.students.last.create_student_info(subject: current_user.client_info.subject, academic_type: student_academic_type)
-      end
-      if current_user.is_tutor? && !tutor_subject_params.blank?
-        ((tutor_subject_params).first[1]).each do |subject_id|
-          current_user.subjects << Subject.find(subject_id.to_i)
-        end
       end
 
     else redirect_back(fallback_location: (request.referer || root_path),
@@ -33,8 +28,6 @@ class UsersController < Clearance::SessionsController
   def enable_user
     if current_user.is_student?
       EngagementCreator.new(current_user, client_as_student_info_params, false).perform
-    elsif current_user.is_tutor?
-      #enable the tutor here if doing it through tutor_params is unsafe or incorrect
     else
       EngagementCreator.new(current_user, nil, true).perform
       if current_user.students.last.email.present?
@@ -47,8 +40,6 @@ class UsersController < Clearance::SessionsController
   def user_params
     if current_user.is_student?
       client_as_student_params
-    elsif current_user.is_tutor?
-      tutor_params
     else
       client_with_student_params
     end
@@ -56,14 +47,6 @@ class UsersController < Clearance::SessionsController
 
 def client_as_student_info_params
     params.require(:info).permit(:academic_type, :subject)
-  end
-
-  def tutor_params
-    params.require(:user).permit(:name, :email, :phone_number, :password).merge(access_state: "enabled")
-  end
-
-  def tutor_subject_params
-    params.fetch(:tutor, {}).permit(subjects: [])
   end
 
   def client_with_student_params
@@ -79,8 +62,4 @@ def client_as_student_info_params
     params.require(:user).permit(:name, :email, :phone_number, :password, client_info_attributes: [:id])
   end
 
-  def tutor_params
-    params.require(:user).permit(:name, :email, :phone_number, :password).merge(access_state: "enabled")
-  end
-  
 end
