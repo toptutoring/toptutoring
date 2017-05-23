2618c93d0cdfb4bc2b9cdcc65d20d786fe06c093
Christian Hecker <christianhecker36@yahoo.com>
2017-05-23 17:42:49 -0500

    Improve availability form and availability controller.
    Add theme and structure to the availability form and add a
    messy, but working, create action to the controller.

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -1,9 +1,75 @@
 class AvailabilityController < ApplicationController
-  
+
+  DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+
   def new
     @days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+    @availabilities = []
+    7.times do
+      @availabilities << Availability.new
+    end
   end
 
   def create
+
+    @days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+    @day_hash = [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+    @availabilities = []
+
+    7.times do
+      @availabilities << Availability.new(from: DateTime.new, to: DateTime.new)
+    end
+
+    @availabilities.each do |a|
+
+      i = @availabilities.index(a)
+
+      a.update_attribute(:from, a.from.beginning_of_week(start_day = @day_hash[i]))
+      a.update_attribute(:to, a.to.beginning_of_week(start_day = @day_hash[i]))
+
+      a.update_attribute(:from, a.from.beginning_of_week(start_day = @day_hash[i]))
+      a.update_attribute(:to, a.to.beginning_of_week(start_day = @day_hash[i]))
+
+
+      from_time_sched = (params[:info]["#{(@days[i])}_from"]+params[:info]["#{(@days[i])}_from_am_pm"]).to_time
+      to_time_sched = (params[:info]["#{(@days[i])}_to"]+params[:info]["#{(@days[i])}_to_am_pm"]).to_time
+
+
+      if !(from_time_sched.nil?)
+        a.update_attribute(:from, a.from.change({hour: from_time_sched.hour, min: from_time_sched.min, sec: from_time_sched.sec}))
+      end
+
+      if !(to_time_sched.nil?)
+        a.update_attribute(:to, a.to.change({hour: to_time_sched.hour, min: to_time_sched.min, sec: to_time_sched.sec}))
+      end
+
+      if !a.save
+        flash[:danger] = "There was an error in your submission."
+        render 'new'
+      end
+    end
+
+    flash[:notice] = "Your submission for (Engagement) was successful."
+    redirect_to profile_path
   end
+
+#   private
+#
+#
+#  def availability_params
+#
+#    permit_strings = []
+#
+#    7.times do |count|
+#      permit_strings << "#{DAYS[count]}_from"
+#      permit_strings << "#{DAYS[count]}_to"
+#      permit_strings << "#{DAYS[count]}_from_am_pm"
+#      permit_strings << "#{DAYS[count]}_to_am_pm"
+#    end
+#
+#    permit_strings = permit_strings.map &:to_sym
+#    STDOUT.puts(permit_strings)
+#
+#    params.require(:info).permit(permit_strings)
+#  end
 end
