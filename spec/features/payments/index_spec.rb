39e8f5791da30241a19939dab4beaa2c7fc9d8b5
Alexandra Persea <perseaalexandra@gmail.com>
2017-05-26 18:53:25 +0300

    Fix last test

--- spec/features/payments/index_spec.rb
+++ spec/features/payments/index_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature "Index Payments" do
   let(:client) { FactoryGirl.create(:client_user) }
-  let(:payment) { FactoryGirl.create(:payment, payee_id: client, amount: 200,
+  let(:payment) { FactoryGirl.create(:payment, payer_id: client.id, amount: 200,
                               status: "paid", description: "initial payment") }
 
   scenario "show all user's past payments" do
