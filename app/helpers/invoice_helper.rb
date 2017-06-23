047ecb21df710221a411825c61abacde0dfeee89
James Hong <ojameso@hotmail.com>
2017-06-23 13:59:10 -0700

    Changed invoice options to show valid options

--- app/helpers/invoice_helper.rb
+++ app/helpers/invoice_helper.rb
@@ -2,17 +2,17 @@ module InvoiceHelper
   def create_select_options_for_invoice
     group = @tutor_engagements.map { |engagement| engagement.student_id }.uniq
     group.map do |s_id|
-      array_for_options(s_id)
+      array_for_options(s_id, current_user)
     end
   end
   
-  def array_for_options(id)
+  def array_for_options(id, tutor)
     student = User.find(id)
-    [student.name, engagement_options(student)]
+    [student.name, engagement_options(student, tutor)]
   end
 
-  def engagement_options(student)
-    student.student_engagements.map do |engagement|
+  def engagement_options(student, tutor)
+    student.student_engagements.where(tutor_id: tutor.id).map do |engagement|
       ["#{engagement.academic_type} for #{engagement.subject}", engagement.id]
     end
   end
