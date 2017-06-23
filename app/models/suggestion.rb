316ff87a7cfb28632bcbfc4bca878e1cb41c9fa7
James Hong <ojameso@hotmail.com>
2017-06-23 13:18:26 -0700

    Removed integer valdiation for suggestion

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,5 +1,5 @@
 class Suggestion < ApplicationRecord
   belongs_to :engagement 
 
-  validates :number_of_hours, presence: true, :numericality =>{ :only_integer => true, :greater_than => 0 }
+  validates :number_of_hours, presence: true, :numericality =>{ :greater_than => 0 }
 end
