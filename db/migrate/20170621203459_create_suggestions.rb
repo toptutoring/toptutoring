adf02cb39816f3614842fd37094baa96b2242017
James Hong <ojameso@hotmail.com>
2017-06-23 12:52:10 -0700

    Fixed spelling errors and format

--- db/migrate/20170621203459_create_suggestions.rb
+++ db/migrate/20170621203459_create_suggestions.rb
@@ -2,8 +2,7 @@ class CreateSuggestions < ActiveRecord::Migration[5.0]
   def change
     create_table :suggestions do |t|
       t.integer :number_of_hours
-      
-      t.references :engagement
+      @client = User.find(@engagement.client_id)      t.references :engagement
 
       t.timestamps
     end
