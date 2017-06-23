a8aa6a0f81af7b8fe66437ef659ab6c30ad6be6d
James Hong <ojameso@hotmail.com>
2017-06-22 19:58:40 -0700

    Added suggest hours as part of Invoice form
    In addition to adding suggest hours to the Invoice form, I also added
    logic into the invoice controller to create a new Suggestion. I also did
    some refactoring to the invoice form and fixed some errors I saw in
    choosing students.

--- app/controllers/tutors/invoices_controller.rb
+++ app/controllers/tutors/invoices_controller.rb
@@ -9,6 +9,15 @@ module Tutors
 
     def create
       @invoice = Invoice.new(invoice_params)
+      
+      if params[:suggestion][:number_of_hours] != ''
+        @suggestion = Suggestion.new(suggestion_params)
+        unless @suggestion.save
+          redirect_back(fallback_location: (request.referer || root_path),
+                        flash: { error: [@suggestion.errors.full_messages, ". Invoice was not created."].join(' ') }) and return
+        end
+      end
+
       if @invoice.save
         @invoice.cancelled! if @invoice.hours == 0
         credit = CreditUpdater.new(@invoice.id).process!
@@ -17,7 +26,7 @@ module Tutors
                         has a negative balance of hours. You may not be paid for this session
                         unless the client adds to his/her hourly balance.' and return
         else
-          redirect_to tutors_students_path, notice: 'Session successfully logged!' and return
+          redirect_to tutors_invoices_path, notice: 'Session successfully logged!' and return
         end
       else
         redirect_back(fallback_location: (request.referer || root_path),
@@ -27,14 +36,20 @@ module Tutors
 
     private
     def invoice_params
-      if params[:academic_type].casecmp('academic') == 0
+      if @engagement.academic_type == 'Academic'
         hourly_rate = MultiCurrencyAmount.from_cent(@client.academic_rate.cents, MultiCurrencyAmount::APP_DEFAULT_CURRENCY)
       else
         hourly_rate = MultiCurrencyAmount.from_cent(@client.test_prep_rate.cents, MultiCurrencyAmount::APP_DEFAULT_CURRENCY)
       end
       params.require(:invoice)
-        .permit(:client_id, :hours, :subject, :description)
-        .merge(tutor_id: current_user.id, hourly_rate: hourly_rate, engagement_id: @engagement.id)
+        .permit(:client_id, :hours, :engagement_id, :subject, :description)
+        .merge(tutor_id: current_user.id, hourly_rate: hourly_rate)
+    end
+
+    def suggestion_params
+      params.require(:suggestion)
+        .permit(:number_of_hours)
+        .merge(engagement_id: @engagement.id)
     end
 
     def set_client
@@ -42,7 +57,7 @@ module Tutors
     end
 
     def set_engagement
-      @engagement = Engagement.find_by(tutor_id: current_user.id, client_id: @client.id, academic_type: params[:academic_type])
+      @engagement = Engagement.find_by(id: params[:invoice][:engagement_id])
     end
 
     def authorize_tutor
