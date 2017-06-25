1bcd0ae1ad70969da3ad578b133516b5c1ad703d
James Hong <ojameso@hotmail.com>
2017-06-24 23:49:56 -0700

    Removed unnecessary authentication method

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,6 @@
 class DashboardsController < ApplicationController
   before_action :require_login
   before_action :build_student_for_client, only: [:client]
-  before_action :check_dwolla_authentication
 
   def admin
     @pending_engagements = Engagement.pending
@@ -23,14 +22,6 @@ class DashboardsController < ApplicationController
   def build_student_for_client
     if !current_user.is_student?
       current_user.students.build
-    end
-  end
-
-  def check_dwolla_authentication
-    begin
-      current_user.has_external_auth?
-    rescue
-      redirect_to profile_path(dwolla_error: true)
     end
   end
 end
