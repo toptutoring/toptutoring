39f2b5a14515fd8a310f39fa9a6c6d58e3e1729a
James Hong <ojameso@hotmail.com>
2017-06-27 12:17:36 -0700

    Added hours and date validation

--- app/controllers/employees/timesheets_controller.rb
+++ app/controllers/employees/timesheets_controller.rb
@@ -3,11 +3,16 @@ module Employees
     before_action :require_login
 
     def create
-      if current_user.timesheets.create(timesheet_params)
-        redirect_to employees_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
+      if hours_valid? && date_valid?
+        if current_user.timesheets.create(timesheet_params)
+          redirect_to employees_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
+        else
+          redirect_back(fallback_location: (request.referer || root_path),
+                        flash: { error: @invoice.errors.full_messages }) and return
+        end
       else
-        redirect_back(fallback_location: (request.referer || root_path),
-                      flash: { error: @invoice.errors.full_messages }) and return
+          redirect_back(fallback_location: (request.referer || root_path),
+                        flash: { error: "PLease input valid hours in order to submit your timesheet." }) and return
       end
     end
 
@@ -20,12 +25,17 @@ module Employees
     end
 
     def update
-      @timesheet = current_user.timesheets.find(params[:id])
-      if @timesheet.update(timesheet_params)
-        redirect_to(employees_timesheets_path, notice: 'Your timesheet has been updated') and return
+      if hours_valid? && date_valid?
+        @timesheet = current_user.timesheets.find(params[:id])
+        if @timesheet.update(timesheet_params)
+          redirect_to(employees_timesheets_path, notice: 'Your timesheet has been updated') and return
+        else
+          redirect_back(fallback_location: (request.referer || root_path),
+                        flash: { error: @invoice.errors.full_messages }) and return
+        end
       else
-        redirect_back(fallback_location: (request.referer || root_path),
-                      flash: { error: @invoice.errors.full_messages }) and return
+          redirect_back(fallback_location: (request.referer || root_path),
+                        flash: { error: "PLease input valid hours in order to update your timesheet." }) and return
       end
     end
     
@@ -46,6 +56,15 @@ module Employees
       params.require(:timesheet)
         .permit(:hours, :date, :description)
         .merge(status: "pending")
+    end
+    
+    def hours_valid?
+      hours_divided = params[:timesheet][:hours].to_f / 0.25
+      hours_divided == hours_divided.to_i
+    end
+
+    def date_valid?
+      params[:timesheet][:date].to_date <= Date.today
     end
   end
 end
