cfcde798e5abd13c325ec9b96fee12f503337bd8
James Hong <ojameso@hotmail.com>
2017-06-27 02:26:37 -0700

    Added timesheet controller for admin

---
+++ app/controllers/admin/timesheets_controller.rb
@@ -0,0 +1,29 @@
+module Employees
+  class TimesheetsController < ApplicationController
+    before_action :require_login
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def index
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+    
+    def destroy
+
+    end
+  end
+end
