3e90a7d5b32cc6cb7e24891ebc99a7dea685bb61
James Hong <ojameso@hotmail.com>
2017-07-05 15:54:10 -0700

    Validations were moved into the Model
    Certain complex conditionals were moved to the model to be handled
    there. Validation mehods were written on the model rather than the
    controller. Before actions were set for the update aand destroy aciton
    so that paid invoices cannot be altered.

--- app/controllers/employees/timesheets_controller.rb
+++ app/controllers/employees/timesheets_controller.rb
@@ -1,18 +1,14 @@
 module Employees
   class TimesheetsController < ApplicationController
     before_action :require_login
+    before_action :check_if_update_possible, only: [:update, :destroy]
 
     def create
-      if hours_valid? && date_valid?
-        if current_user.timesheets.create(timesheet_params)
-          redirect_to employees_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
-        else
-          redirect_back(fallback_location: (request.referer || root_path),
-                        flash: { error: @invoice.errors.full_messages }) and return
-        end
+      if current_user.timesheets.create(timesheet_params)
+        redirect_to employees_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
       else
-          redirect_back(fallback_location: (request.referer || root_path),
-                        flash: { error: "Please input valid hours in order to submit your timesheet." }) and return
+        redirect_back(fallback_location: (request.referer || root_path),
+                      flash: { error: @invoice.errors.full_messages }) and return
       end
     end
 
@@ -21,30 +17,20 @@ module Employees
     end
 
     def edit
-      if not_paid?
-        @timesheet = current_user.timesheets.find(params[:id])
-      else
-        redirect_to(employees_timesheets_path, notice: 'This invoice is already paid and can not be updated.') and return
-      end
+      @timesheet = current_user.timesheets.find(params[:id])
     end
 
     def update
-      if hours_valid? && date_valid? && not_paid?
-        @timesheet = current_user.timesheets.find(params[:id])
-        if @timesheet.update(timesheet_params)
-          redirect_to(employees_timesheets_path, notice: 'Your timesheet has been updated') and return
-        else
-          redirect_back(fallback_location: (request.referer || root_path),
-                        flash: { error: @invoice.errors.full_messages }) and return
-        end
+      @timesheet = current_user.timesheets.find(params[:id])
+      if @timesheet.update(timesheet_params)
+        redirect_to(employees_timesheets_path, notice: 'Your timesheet has been updated') and return
       else
-          redirect_back(fallback_location: (request.referer || root_path),
-                        flash: { error: "Please input valid hours in order to update your timesheet." }) and return
+        redirect_back(fallback_location: (request.referer || root_path),
+                      flash: { error: @invoice.errors.full_messages }) and return
       end
     end
-    
+
     def destroy
-      @timesheet = current_user.timesheets.find(params[:id])
       if @timesheet.destroy
         redirect_to(employees_timesheets_path, notice: 'Your timesheet has been deleted.') and return
       else
@@ -58,21 +44,19 @@ module Employees
     def timesheet_params
       params[:timesheet][:hours] = (params[:timesheet][:hours].to_f * 60).to_i
       params.require(:timesheet)
-        .permit(:hours, :date, :description)
-        .merge(status: "pending")
+            .permit(:hours, :date, :description)
+            .merge(status: "pending")
     end
     
-    def hours_valid?
-      hours_divided = params[:timesheet][:hours].to_f / 0.25
-      hours_divided == hours_divided.to_i
-    end
-
-    def date_valid?
-      params[:timesheet][:date].to_date <= Date.today
-    end
-
     def not_paid?
       current_user.timesheets.find(params[:id]).status != "paid"
+    end
+
+    def check_if_update_possible
+        @timesheet = current_user.timesheets.find(params[:id])
+        if @timesheet.status == "paid"
+          redirect_to(employees_timesheets_path, notice: 'This invoice is already paid and can not be updated.') and return
+        end
     end
   end
 end
--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,3 +1,13 @@
 class Timesheet < ApplicationRecord
   belongs_to :users
+  validate :hours_must_be_quarter_hours, :date_must_be_before_today
+
+  def hours_must_be_quarter_hours
+    hours_divided = hours.to_f * 4
+    errors.add(:hours, "must be in quarter hours") unless hours_divided == hours_divided.to_i
+  end
+
+  def date_must_be_before_today
+    errors.add(:date, "may not be in the future.") unless date.to_date <= Date.today.end_of_day
+  end
 end
