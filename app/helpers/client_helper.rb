c9131d818b888dcb1798fc19a26b8eb4b425cc02
Christian Hecker <christianhecker36@yahoo.com>
2017-07-06 17:06:45 -0500

    Add a quick-fix helper to format telephone numbers.

--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -4,4 +4,21 @@ module ClientHelper
       "#{client.academic_credit + client.test_prep_credit} hrs credit"
     end
   end
+
+  def cleanup_phone_number number
+    if number.nil?
+      "No phone number listed."
+    else
+      extract_phone_number(number)
+    end
+  end
+
+  private
+
+  def extract_phone_number number
+    extracted_num = number.split(//).map {|x| x[/\d+/]}.compact.join("")
+    "(#{extracted_num[0..2]}) #{extracted_num[3..5]}-#{extracted_num[6..9]}"
+  end
+
+
 end
