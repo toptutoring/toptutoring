ca10be60b0541cbb7fd7df873d5f66d6b28d10a0
James Hong <ojameso@hotmail.com>
2017-07-13 13:48:25 -0700

    Changed all reference to employee to contractor
    Employee was the wrong term to be used for the timesheets feature. The
    role was properly categorized as contractor and all necessary code
    references were changed.

--- app/controllers/contractors/timesheets_controller.rb
+++ app/controllers/contractors/timesheets_controller.rb
@@ -1,4 +1,4 @@
-module Employees
+module Contractors
   class TimesheetsController < ApplicationController
     before_action :require_login
     before_action :check_if_update_possible, only: [:update, :destroy]
@@ -6,7 +6,7 @@ module Employees
     def create
       @timesheet = current_user.timesheets.new(timesheet_params) 
       if @timesheet.save
-        redirect_to employees_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
+        redirect_to contractors_timesheets_path, notice: 'Thank you for submitting your timesheet.' and return
       else
         redirect_back(fallback_location: (request.referer || root_path),
                       flash: { error: @timesheet.errors.full_messages }) and return
@@ -24,7 +24,7 @@ module Employees
     def update
       @timesheet = current_user.timesheets.find(params[:id])
       if @timesheet.update(timesheet_params)
-        redirect_to(employees_timesheets_path, notice: 'Your timesheet has been updated') and return
+        redirect_to(contractors_timesheets_path, notice: 'Your timesheet has been updated') and return
       else
         redirect_back(fallback_location: (request.referer || root_path),
                       flash: { error: @timesheet.errors.full_messages }) and return
@@ -33,7 +33,7 @@ module Employees
 
     def destroy
       if @timesheet.destroy
-        redirect_to(employees_timesheets_path, notice: 'Your timesheet has been deleted.') and return
+        redirect_to(contractors_timesheets_path, notice: 'Your timesheet has been deleted.') and return
       else
         redirect_back(fallback_location: (request.referer || root_path),
                       flash: { error: @timesheet.errors.full_messages }) and return
@@ -56,7 +56,7 @@ module Employees
     def check_if_update_possible
         @timesheet = current_user.timesheets.find(params[:id])
         if @timesheet.status == "paid"
-          redirect_to(employees_timesheets_path, notice: 'This timesheet is already paid and can not be updated.') and return
+          redirect_to(contractors_timesheets_path, notice: 'This timesheet is already paid and can not be updated.') and return
         end
     end
   end
