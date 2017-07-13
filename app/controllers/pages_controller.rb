2e8f953251d91116c391fde4cbe299c552caf5f9
Christian Hecker <christianhecker36@yahoo.com>
2017-07-13 18:46:22 -0500

    Initial run at updating the payment page.
    Users will be notified if their balance is low or if they
    have not paid for first tutoring sessions. Users will have
    the option to pay on the page and an AJAX-triggered, prefilled
    payment form will appear for the user.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,4 +15,40 @@ class PagesController < ApplicationController
   def calendar
     render "calendar", :layout => false
   end
+
+  def payment
+    @student_engagements = current_user.student_engagements
+    @invoices = []
+
+    @total_suggested_hours = 0
+    @student_engagements.each do |engagement|
+      invoices = Invoice.where(engagement_id: engagement.id)
+      if invoices.count == 1
+        if invoices.first.status != "paid"
+          @invoices << invoices.first
+          @total_suggested_hours += invoices.first.suggested_hours unless invoices.first.suggested_hours.nil?
+        end
+      end
+    end
+
+
+  end
+
+  def first_session_payment
+
+    @invoice = Invoice.find(params[:invoice])
+
+    respond_to do |format|
+      format.js { render :file => 'pages/first_session_payment.js.erb' }
+    end
+  end
+
+  def low_balance_payment
+
+    @invoice = Invoice.find(params[:invoice])
+
+    respond_to do |format|
+      format.js { render :file => 'pages/low_balance_payment.js.erb' }
+    end
+  end
 end
