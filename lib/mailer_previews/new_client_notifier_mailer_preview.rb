9a675efbb91a6044cfd42cfcfacf23664172c941
Alexandra Persea <perseaalexandra@gmail.com>
2017-08-03 16:45:59 +0300

    Add date of signup to signup emails.

--- lib/mailer_previews/new_client_notifier_mailer_preview.rb
+++ lib/mailer_previews/new_client_notifier_mailer_preview.rb
@@ -1,7 +1,7 @@
 class NewClientNotifierMailerPreview < ActionMailer::Preview
   def welcome
     client_info = Struct.new(:subject, :comments).new("Math", nil)
-    client = Struct.new(:email, :name, :client_info).new("client@example.com", "Client", client_info)
+    client = Struct.new(:email, :name, :created_at, :client_info).new("client@example.com", "Client", Time.current, client_info, )
     admin = Struct.new(:email, :name).new("admin@example.com", "Admin")
     NewClientNotifierMailer.welcome(client, admin)
   end
