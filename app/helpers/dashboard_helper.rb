7da94b53e5cd4df782a8866fd778e94f3f7f6d5a
Christian Hecker <christianhecker36@yahoo.com>
2017-08-25 14:43:32 -0500

    Fix issues brought up within the PR comments.
    Remove unnecessary dashboard titles and helpers. Add a conditional
    to decide whether pre-pay option should be presented to clients
    on their dashboard.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,13 +3,4 @@ module DashboardHelper
     "Hello #{current_user.name}, your #{ current_user.has_role?("director") ? 'administrator' : 'director' }
       is using Dwolla in order to make transfers. Please click the button below to authenticate with Dwolla."
   end
-
-  def any_engagements_active? engagements
-    engagements.each do |engagement|
-      if engagement.active?
-        return true
-      end
-    end
-  false
-  end
 end
