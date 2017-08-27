191512d5220eb7cb2f211f3079ae99e72c9f08d2
Christian Hecker <christianhecker36@yahoo.com>
2017-08-27 04:27:54 -0500

    Fix tests breaking due to nil class.

--- spec/features/payments/form_spec.rb
+++ spec/features/payments/form_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 feature "Navigate to payment as client" do
   let(:client) { FactoryGirl.create(:client_user) }
+  let(:engagement) { FactoryGirl.create(:engagement, client: client, student_name: "student")}
 
   scenario "with valid payment form" do
+    engagement
     sign_in(client)
 
     expect(page.current_path).to eq dashboard_path
--- spec/features/payments/index_spec.rb
+++ spec/features/payments/index_spec.rb
@@ -4,8 +4,10 @@ feature "Index Payments" do
   let(:client) { FactoryGirl.create(:client_user) }
   let(:payment) { FactoryGirl.create(:payment, payer_id: client.id, amount_in_cents: 20000,
                               status: "paid", description: "initial payment") }
+  let(:engagement) { FactoryGirl.create(:engagement, client: client, student_name: "student")}
 
   scenario "show all user's past payments" do
+    engagement
     payment
 
     sign_in(client)
