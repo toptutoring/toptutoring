1c7a4e75776891328770e2ac9433b9c52748e3d4
Christian Hecker <christianhecker36@yahoo.com>
2017-09-02 20:09:54 -0500

    Account for multiple engagements.
    Remove errors arising from a client having multiple
    engagements. Add a method to select the proper
    engagement tutoring type.

--- app/helpers/payment_helper.rb
+++ app/helpers/payment_helper.rb
@@ -1,14 +1,18 @@
 module PaymentHelper
 
   def get_tutor_rate(engagement)
-    if engagement.academic_type == "Academic"
-      engagement.tutor.academic_rate_cents/100.0
-    else
-      engagement.tutor.test_prep_rate_cents/100.0
+    if !(engagement.tutor.nil?)
+      if engagement.academic_type == "Academic"
+        engagement.tutor.academic_rate_cents/100.0
+      else
+        engagement.tutor.test_prep_rate_cents/100.0
+      end
     end
   end
 
   def get_suggested_hours(engagement)
-      engagement.suggestions.last.hours
+      if !(engagement.suggestions.nil?)
+        engagement.suggestions.last.hours
+      end
   end
 end
