593afb3d4e0fba9f3359ac9bb189dea986529987
Christian Hecker <christianhecker36@yahoo.com>
2017-09-10 01:37:56 -0500

    Properly select tutoring type on signup.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,18 +3,4 @@ module DashboardHelper
     "Hello #{current_user.name}, your #{ current_user.has_role?("director") ? 'administrator' : 'director' }
       is using Dwolla in order to make transfers. Please click the button below to authenticate with Dwolla."
   end
-
-  def get_academic_type_from_subject
-    subject = current_user.client_info.subject
-
-    if(subject.include?("ACT") || subject.include?("PSAT") || subject.include?("CASHSEE") || subject.include?("CHSPE") ||
-      subject.include?("CLEP") || subject.include?("CPA") || subject.include?("EOC") || subject.include?("GED") || subject.include?("GMAT") ||
-      subject.include?("GRE") || subject.include?("PCAT") || subject.include?("SAT") || subject.include?("TOEFL") || subject.include?("TOPS") ||
-      subject.include?("WISC") || subject.include?("WPPSI"))
-
-      "Test_Prep"
-    else
-      "Academic"
-    end
-  end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,14 @@
 class Subject < ActiveRecord::Base
   validates_uniqueness_of :name
 
+  TEST_PREP_SUBJECTS = %w(ACT PSAT CASHEE CHSPE
+                          CLEP CPA EOC GED GMAT
+                          GRE PCAT SAT TOEFL TOPS
+                          WISC WPPSI).freeze
+
   belongs_to :user
+
+  def test_prep?
+    (TEST_PREP_SUBJECTS.map { |subject| name.include?(subject) }).include?(true)
+  end
 end
