9652c6dba0d21690952a2e82ea6bd8639af0e776
James Hong <ojameso@hotmail.com>
2017-11-16 22:21:11 -0800

    Makes mailer take id instead of user
    The Mailer was taking the user object as an argument. This can cause issues later if this was being queued by sidekiq. Therefore, the method was changed to take an id.

--- app/mailers/set_student_password_mailer.rb
+++ app/mailers/set_student_password_mailer.rb
@@ -1,8 +1,8 @@
 class SetStudentPasswordMailer < ApplicationMailer
   default from: 'tutor@toptutoring.org'
 
-  def set_password(user)
-    @user = user
+  def set_password(user_id)
+    @user = User.find(user_id)
     mail(to: @user.email,
          subject: "Welcome to Top Tutoring")
   end
