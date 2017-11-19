39b39d3830a34e5d7ad4b270e02bfa2526e66297
James Hong <ojameso@hotmail.com>
2017-11-18 16:45:27 -0800

    Removes logic for current_user in auth layout
    Authentication layout assumes there is no one signed in. Therefore,
    logic to check if the user is signed in has been removed.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,6 +2,8 @@ class PasswordsController < Clearance::PasswordsController
   layout "authentication"
 
   def edit
+    # Sign out any current user that might already be logged in
+    # to avoid signing in multiple users after password reset
     sign_out if current_user
     @user = User.find(params[:user_id])
   end
