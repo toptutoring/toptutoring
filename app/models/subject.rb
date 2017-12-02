5b01261437a33e45d32d4fd13e90b7ec299c2ed8
James Hong <ojameso@hotmail.com>
2017-12-02 11:43:29 -0800

    Adds name validation to subejcts

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,5 @@
 class Subject < ActiveRecord::Base
+  validates_presence_of :name
   validates_uniqueness_of :name
   enum academic_type: { academic: "academic", 
                         test_prep: "test_prep" }
