9cc1316f19a4d0931124906e1b5f53f1e247bd89
James Hong <ojameso@hotmail.com>
2017-12-13 10:25:09 -0800

    Menu link checks for account rather than signup

--- app/helpers/menu_links_helper.rb
+++ app/helpers/menu_links_helper.rb
@@ -1,11 +1,11 @@
 module MenuLinksHelper
   def link_to_client_students_or_tutors
-    if current_user.is_student?
+    if current_user.student_account
       link_to clients_tutors_path do
         tag.i(class: "icon ion-ios-contact bg-purple") +
           content_tag(:span, "Your Tutors", class: "sidebar-title")
       end
-    else
+    elsif current_user.client_account
       link_to clients_students_path do
         tag.i(class: "icon ion-ios-contact bg-purple") +
           content_tag(:span, "Your Students", class: "sidebar-title")
