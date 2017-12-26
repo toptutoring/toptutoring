65062ec2d1bd6cd44ea06306072ce45e29e1993b
James Hong <ojameso@hotmail.com>
2017-12-26 13:43:59 -0800

    Updates spec to handle additions to Payment

--- spec/services/payment_service_spec.rb
+++ spec/services/payment_service_spec.rb
@@ -2,15 +2,18 @@ require "rails_helper"
 
 describe CreateClientService do
     let(:client) { FactoryBot.create(:client_user) }
+    let(:stripe_obj) { Struct.new(:stripe_token, :last_four, :card_brand)
+                             .new("tok_visa", "0123", "Visa") }
   describe "#charge!" do
     it "successfully charges client" do
-      subject = PaymentService.new(client, "academic", 2.0).charge!("tok_visa")
+      subject = PaymentService.new(client, "academic", 2.0).charge!(stripe_obj)
 
       expect(subject.success?).to be true
     end
 
     it "fails with bad hours" do
-      subject = PaymentService.new(client, "academic", 1.11).charge!("waefwaefaw")
+      stripe_obj.stripe_token = "bad_token"
+      subject = PaymentService.new(client, "academic", 1.11).charge!(stripe_obj)
 
       expect(subject.success?).to be false
     end
@@ -18,7 +21,7 @@ describe CreateClientService do
     it "successfully adjusts client academic credit" do
       subject = client.test_prep_credit
       hours = 2.0
-      PaymentService.new(client, "academic", hours).charge!("tok_visa")
+      PaymentService.new(client, "academic", hours).charge!(stripe_obj)
 
       expect(subject).to eq(client.reload.academic_credit - hours)
     end
@@ -26,7 +29,7 @@ describe CreateClientService do
     it "successfully adjusts client test prep credit" do
       subject = client.test_prep_credit
       hours = 1.25
-      PaymentService.new(client, "test_prep", hours).charge!("tok_visa")
+      PaymentService.new(client, "test_prep", hours).charge!(stripe_obj)
 
       expect(subject).to eq(client.reload.test_prep_credit - hours)
     end
