f4ee416fd3432569d880d19db365bf83fb7ae03c
James Hong <ojameso@hotmail.com>
2018-01-02 14:22:21 -0800

    Refactors menu links and adds active link

--- app/helpers/menu_links_helper.rb
+++ app/helpers/menu_links_helper.rb
@@ -1,15 +1,22 @@
 module MenuLinksHelper
   def link_to_client_students_or_tutors
     if current_user.student_account
-      link_to clients_tutors_path do
-        tag.i(class: "icon ion-ios-contact bg-purple") +
-          content_tag(:span, "Your Tutors", class: "sidebar-title")
-      end
+      menu_link(clients_tutors_path, "Your Tutors", "icon ion-ios-contact bg-purple")
     elsif current_user.client_account
-      link_to clients_students_path do
-        tag.i(class: "icon ion-ios-contact bg-purple") +
-          content_tag(:span, "Your Students", class: "sidebar-title")
+      menu_link(clients_students_path, "Your Students", "icon ion-ios-contact bg-purple")
+    end
+  end
+
+  def menu_link(path, title, icon_style)
+    tag.li class: "panel" do
+      link_to path, class: active?(path) do
+        tag.i(class: icon_style) +
+        tag.span(title, class: "sidebar-title")
       end
     end
+  end
+
+  def active?(path)
+    request.path == path ? "active" : nil
   end
 end
