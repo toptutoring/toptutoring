da1491434a0ead0ebdc83ab6133eb6338189a255
James Hong <ojameso@hotmail.com>
2018-01-08 23:10:49 -0800

    Configure ckeditor to work with carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,18 @@ ActiveRecord::Schema.define(version: 20180109193713) do
     t.index ["user_id"], name: "index_blog_posts_on_user_id"
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
   create_table "client_accounts", force: :cascade do |t|
     t.bigint "user_id"
     t.datetime "created_at", null: false
