e7a7e54de345e6ae374da13dc30a5f0bcd7bae63
James Hong <ojameso@hotmail.com>
2018-01-08 21:41:16 -0800

    Updates spec to handle purchase info in payment

--- spec/services/payment_service_spec.rb
+++ spec/services/payment_service_spec.rb
@@ -2,38 +2,40 @@ require "rails_helper"
 
 describe CreateClientService do
   let(:client) { FactoryBot.create(:client_user) }
-  let(:purchase_params) { { academic_type: "academic", hours_desired: "2.0", stripe_token: "tok_visa",
-                            card_brand: "Visa", last_four: "4242", card_holder_name: "Client" } }
+  let(:payment_params) { { hours_type: "academic", hours_purchased: "2.0",
+                            card_brand: "Visa", last_four: "4242", rate: 20_00,
+                            card_holder_name: "Client", payer_id: client.id } }
+  let(:token) { { stripe_token: "tok_visa" } }
 
   describe "#charge!" do
     it "successfully charges client" do
-      subject = PaymentService.new(client, purchase_params).charge!
+      subject = PaymentService.new(payment_params, token).charge!
 
       expect(subject.success?).to be true
     end
 
     it "fails with bad hours" do
-      purchase_params[:hours_desired] = "1.11"
-      subject = PaymentService.new(client, purchase_params).charge!
+      payment_params[:hours_purchased] = "1.11"
+      subject = PaymentService.new(payment_params, token).charge!
 
       expect(subject.success?).to be false
     end
 
     it "successfully adjusts client academic credit" do
       subject = client.test_prep_credit
-      PaymentService.new(client, purchase_params).charge!
+      PaymentService.new(payment_params, token).charge!
 
-      hours = purchase_params[:hours_desired].to_f
+      hours = payment_params[:hours_purchased].to_f
       expect(subject).to eq(client.reload.academic_credit - hours)
     end
 
     it "successfully adjusts client test prep credit" do
       subject = client.test_prep_credit
-      purchase_params[:hours_desired] = "1.25"
-      purchase_params[:academic_type] = "test_prep"
-      PaymentService.new(client, purchase_params).charge!
+      payment_params[:hours_purchased] = "1.25"
+      payment_params[:hours_type] = "test_prep"
+      PaymentService.new(payment_params, token).charge!
 
-      hours = purchase_params[:hours_desired].to_f
+      hours = payment_params[:hours_purchased].to_f
       expect(subject).to eq(client.reload.test_prep_credit - hours)
     end
   end
