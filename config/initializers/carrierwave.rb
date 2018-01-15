9921e7eb84e02df5b67085ddcdb82fa36e6410ad
James Hong <ojameso@hotmail.com>
2018-01-14 23:05:55 -0800

    Adds Amazon S3 for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,19 @@
 CarrierWave.configure do |config|
-  config.storage = :file
+  storage_type = Rails.env.test? ? :file : :fog
 
-  if Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-  end
+  config.fog_provider = "fog/aws"
+  config.fog_credentials = {
+    provider:              "AWS",
+    aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY"),
+    aws_secret_access_key: ENV.fetch("AWS_SECRET_KEY"),
+    region:                ENV.fetch("AWS_REGION")
+  }
+  config.fog_directory = ENV.fetch("AWS_S3_BUCKET_NAME")
 
-  if Rails.env.development?
-    config.ignore_integrity_errors = false
-    config.ignore_processing_errors = false
-    config.ignore_download_errors = false
-  end
+  # Add additional options
+  # config.fog_attributes = { cache_control: "publi, max-age=#{365.days.to_i}" }  defaults to {}
+
+  config.storage = storage_type
+  # test configurations
+  config.enable_processing = false if Rails.env.test?
 end
