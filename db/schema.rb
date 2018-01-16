956cff98f4af6bf5327a53ac3e0d68060ea47291
James Hong <ojameso@hotmail.com>
2018-01-15 23:32:02 -0800

    Data migration for payment error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115070830) do
+ActiveRecord::Schema.define(version: 20180116072145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
