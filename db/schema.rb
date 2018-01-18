dc6f72a3cb18986bc1a69255432ae851a40d9d72
James Hong <ojameso@hotmail.com>
2018-01-18 15:35:33 -0800

    Adds published category to cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116072145) do
+ActiveRecord::Schema.define(version: 20180118180546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180116072145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "country_id"
+    t.boolean "published", default: false
     t.index ["country_id"], name: "index_cities_on_country_id"
   end
 
