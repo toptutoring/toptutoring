25c39351f8cf504d1dfa26e52d009639a32f5bd1
James Hong <ojameso@hotmail.com>
2018-01-22 15:52:34 -0800

    Reduces scopes for dwolla permissions

--- app/controllers/auth_callbacks_controller.rb
+++ app/controllers/auth_callbacks_controller.rb
@@ -1,18 +1,12 @@
 class AuthCallbacksController < ApplicationController
+  before_action :require_login
+
   def create
-    user_from_auth_hash
+    AuthHashService.new(auth_hash).update_dwolla_attributes(current_user)
     redirect_to dashboard_path
   end
 
   private
-
-  def user_from_auth_hash
-    if current_user
-      AuthHashService.new(auth_hash).set_token(current_user)
-    else
-      sign_in AuthHashService.new(auth_hash).find_or_create_user_from_auth_hash
-    end
-  end
 
   def auth_hash
     request.env["omniauth.auth"]
