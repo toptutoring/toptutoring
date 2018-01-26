dd2a1fa2c1fcc83998582483bfd138e9bd9edc3e
James Hong <ojameso@hotmail.com>
2018-01-26 15:47:16 -0800

    Updates specs to handle stored cards

--- spec/services/payment_service_spec.rb
+++ spec/services/payment_service_spec.rb
@@ -2,28 +2,27 @@ require "rails_helper"
 
 describe CreateClientService do
   let(:client) { FactoryBot.create(:client_user) }
-  let(:payment_params) { { hours_type: "online_academic", hours_purchased: "2.0",
-                            card_brand: "Visa", last_four: "4242", rate: 20_00,
-                            card_holder_name: "Client", payer_id: client.id } }
-  let(:token) { { stripe_token: "tok_visa" } }
+  let(:payment_params) { { hours_type: "online_academic", hours_purchased: "2.0", payer_id: client.id } }
+  let(:token) { "tok_visa" }
+  let(:stripe_account) { FactoryBot.create(:stripe_account) }
 
   describe "#charge!" do
     it "successfully charges client" do
-      subject = PaymentService.new(payment_params, token).charge!
+      subject = PaymentService.new(token, payment_params, nil).charge!
 
       expect(subject.success?).to be true
     end
 
     it "fails with bad hours" do
       payment_params[:hours_purchased] = "1.11"
-      subject = PaymentService.new(payment_params, token).charge!
+      subject = PaymentService.new(token, payment_params, nil).charge!
 
       expect(subject.success?).to be false
     end
 
     it "successfully adjusts client academic credit" do
-      subject = client.online_test_prep_credit
-      PaymentService.new(payment_params, token).charge!
+      subject = client.test_prep_credit
+      PaymentService.new(token, payment_params, nil).charge!
 
       hours = payment_params[:hours_purchased].to_f
       expect(subject).to eq(client.reload.online_academic_credit - hours)
@@ -33,7 +32,7 @@ describe CreateClientService do
       subject = client.online_test_prep_credit
       payment_params[:hours_purchased] = "1.25"
       payment_params[:hours_type] = "online_test_prep"
-      PaymentService.new(payment_params, token).charge!
+      PaymentService.new(token, payment_params, nil).charge!
 
       hours = payment_params[:hours_purchased].to_f
       expect(subject).to eq(client.reload.online_test_prep_credit - hours)
@@ -47,6 +46,12 @@ describe CreateClientService do
 
       hours = payment_params[:hours_purchased].to_f
       expect(subject).to eq(client.reload.in_person_test_prep_credit - hours)
+    end
+
+    it "successfully charges a stored card" do
+      subject = PaymentService.new(stripe_account.default_source_id, payment_params, stripe_account).charge!
+
+      expect(subject.success?).to be true
     end
   end
 end
