60dccc035b71a8f174c5e4000cff145b07f9e053
James Hong <ojameso@hotmail.com>
2018-02-20 11:17:43 -0800

    Allows admin to manually request reviews

--- app/controllers/admin/client_reviews_controller.rb
+++ app/controllers/admin/client_reviews_controller.rb
@@ -4,4 +4,12 @@ module Admin
       @clients = User.clients
       @reviews = ClientReview.all
     end
+
+    def create
+      client = User.find_by_id(params[:client_id])
+      UserNotifierMailer.send_review_request(client)
+      flash.notice = "Review has been requested."
+      redirect_to admin_client_reviews_path
+    end
+  end
 end
