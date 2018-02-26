233f2547ed0a889e50491d90505099db669c9909
Fred Zhang <awormasi@gmail.com>
2018-02-25 16:09:59 -0800

    Rename files and components to be more descriptive

--- app/javascript/packs/contact-application.js
+++ app/javascript/packs/contact-application.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Home } from '../components/home.component';
-import '../styles/application.scss';
+import '../styles/contact-application.scss';
 
 ReactDOM.render(
     <Home />,
