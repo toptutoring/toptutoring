2820fc5a9b9e7866ad3759103f726e4e9750e877
James Hong <ojameso@hotmail.com>
2018-03-01 13:45:32 -0800

    Adds excerpt to BlogPost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228172000) do
+ActiveRecord::Schema.define(version: 20180301214002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180228172000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.text "excerpt"
     t.index ["published"], name: "index_blog_posts_on_published"
     t.index ["slug"], name: "index_blog_posts_on_slug", unique: true
     t.index ["title"], name: "index_blog_posts_on_title", unique: true
