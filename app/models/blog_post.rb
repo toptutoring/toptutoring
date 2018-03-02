3a492259f5a1cba95b2026d7bd25f634e87d0730
James Hong <ojameso@hotmail.com>
2018-03-02 14:46:43 -0800

    Updates slug formation for blog

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,14 +1,15 @@
 class BlogPost < ApplicationRecord
-  before_save :add_slug
+  before_validation :add_slug
   belongs_to :user
   has_and_belongs_to_many :blog_categories
   validates_presence_of :title, :content, :publish_date, :user
-  validates :title, uniqueness: true
+  validates :title, :slug, uniqueness: true
   
   scope :published, -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
 
   def add_slug
+    return unless title_changed?
     self.slug = title.downcase
                      .gsub(/[^0-9A-Za-z\(\.\s]/, '')
                      .tr(" .(", "-")
