b2edfec598fee7350a6f8fb08d41f97900659b31
James Hong <ojameso@hotmail.com>
2018-03-13 11:18:21 -0700

    Changes actions into icons with tooltips

--- app/helpers/engagement_helper.rb
+++ app/helpers/engagement_helper.rb
@@ -18,11 +18,11 @@ module EngagementHelper
   def rates_and_credits(engagement)
     client = engagement.client
     if engagement.academic?
-      concat tag.p "Online: #{rate_credit_string(client, "online_academic")}"
-      concat tag.p "In-Person: #{rate_credit_string(client, "in_person_academic")}"
+      concat tag.p "Online: #{rate_credit_string(client, "online_academic")}" if engagement.rate_for?("online")
+      concat tag.p "In-Person: #{rate_credit_string(client, "in_person_academic")}" if engagement.rate_for?("in_person")
     else
-      concat tag.p "Online: #{rate_credit_string(client, "online_test_prep")}"
-      concat tag.p "In-Person: #{rate_credit_string(client, "in_person_test_prep")}"
+      concat tag.p "Online: #{rate_credit_string(client, "online_test_prep")}" if engagement.rate_for?("online")
+      concat tag.p "In-Person: #{rate_credit_string(client, "in_person_test_prep")}" if engagement.rate_for?("in_person")
     end
   end
 
@@ -41,5 +41,32 @@ module EngagementHelper
       concat tag.p "Online: #{client.online_test_prep_credit}" if engagement.rate_for?("online")
       concat tag.p "In-Person: #{client.in_person_test_prep_credit}" if engagement.rate_for?("in_person")
     end
+  end
+
+  def actions_for_engagement(engagement)
+    concat edit_link(engagement)    
+    concat misc_action_link(engagement)
+  end
+
+  def edit_link(engagement)
+    link_to edit_engagement_path(engagement), data: { toggle: "tooltip", placement: "top", "original-title" => "Edit this engagement" }, class: "mr-15 fs-24" do 
+      tag.i class: "icon ion-edit"
+    end 
+  end
+
+  def misc_action_link(engagement)
+    if engagement.tutor_account && engagement.pending? 
+       link_to enable_engagement_path(engagement), data: { toggle: "tooltip", placement: "top", "original-title" => "Enable this engagement", confirm: "Enable this engagement?" }, class: "fs-24" do 
+        tag.i class: "icon ion-play"
+      end 
+    elsif engagement.active? 
+      link_to disable_engagement_path(engagement), data: { toggle: "tooltip", placement: "top", "original-title" => "Archive this engagement", confirm: "Archive this engagement? Archiving will disable the tutor from viewing and invoicing this engagement." }, class: "fs-24" do 
+        tag.i class: "icon ion-android-archive"
+      end 
+    else 
+      link_to engagement_path(engagement), remote: true, method: :delete, data: { toggle: "tooltip", placement: "top", "original-title" => "Delete this engagement", confirm: "This will permanently remove the engagement from the databse. Are you sure?" }, class: "fs-24" do 
+        tag.i class: "icon ion-trash-b"
+      end 
+    end 
   end
 end
