b45ee709e807ba0eda1d452e32636c788f7e3fa1
James Hong <ojameso@hotmail.com>
2018-03-21 15:10:53 -0700

    Adds spec for referral feature

---
+++ spec/features/clients/referral_sign_up_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+feature "User signs up as referral" do
+  let(:admin) { FactoryBot.create(:admin_user) }
+  let(:client) { FactoryBot.create(:client_user) }
+  let!(:subject_academic) { FactoryBot.create(:subject) }
+
+  context "with valid params" do
+    scenario "when user is a client that is not a student" do
+      admin # creates admin user to check if email is sent to admin
+      visit referral_path(client.unique_token)
+      expect(page).to have_content(client.full_name)
+
+      count_at_start = client.referrals.count
+      name = "ClientName"
+      last_name = "ClientLastName"
+      fill_in "user_first_name", with: name
+      fill_in "user_last_name", with: last_name
+      fill_in "user_email", with: "client@example.com"
+      fill_in "user_phone_number", with: "(510)555-5555"
+      fill_in "user_password", with: "password"
+      fill_in "confirm_password", with: "password"
+      find("#user_signup_attributes_subject_id").find(:xpath, "option[2]").select_option
+      find("#user_signup_attributes_student").find(:xpath, "option[2]").select_option
+      click_button "Submit"
+
+      expect(page).to have_current_path(new_clients_student_path)
+      expect(page).to have_content(I18n.t("app.signup.client.success_message"))
+      expect(page).to have_content(name)
+      expect(page).to have_content(last_name)
+      # Email count should be 2 since an email is sent to both client and admin
+      expect(ActionMailer::Base.deliveries.count).to eq(2)
+      expect(User.last.referrer).to eq client
+      expect(client.reload.referrals.count).to eq count_at_start + 1
+    end
+  end
+end
---
+++ spec/jobs/referral_job_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe ReferralJob, type: :job do
+  include ActiveJob::TestHelper
+  let(:referrer) { FactoryBot.create(:client_user, in_person_test_prep_credit: 1, in_person_test_prep_rate_cents: 100_00) }
+  let!(:referred_client) { FactoryBot.create(:client_user, referrer: referrer) }
+  subject = ReferralJob
+
+  describe ".perform" do
+    it "adds an hour to referrer and marks referral_claimed to true" do
+      original_credit = referrer.in_person_test_prep_credit
+      subject.perform_now(referred_client.id)
+
+      expect(referred_client.reload.referral_claimed).to be_truthy
+      expect(referrer.reload.in_person_test_prep_credit).to eq original_credit + 1
+      expect(referrer.in_person_academic_credit).to eq 0
+      expect(referrer.online_academic_credit).to eq 0
+      expect(referrer.online_test_prep_credit).to eq 0
+    end
+
+    after do
+      clear_enqueued_jobs
+      clear_performed_jobs
+    end
+  end
+end
