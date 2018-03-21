41a833a9d16bf3f5da1139c2b3fa2594ac0af056
James Hong <ojameso@hotmail.com>
2018-03-20 18:36:23 -0700

    Adds referral credit when invoice is created

---
+++ app/jobs/referral_job.rb
@@ -0,0 +1,30 @@
+class ReferralJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(id)
+    @client = User.find(id)
+    @referrer = @client.referrer
+    return unless perform_referral_update
+  end
+
+  private 
+
+  def perform_referral_update
+    type = @referrer.client_account.highest_rate_type
+    return unless type
+    update_users(type + "_credit")
+  end
+
+  def update_users(credit_type)
+    User.transaction do
+      @referrer.send(credit_type + "=", @referrer.send(credit_type) + 1)
+      @referrer.save!
+      @client.referral_claimed = true
+      @client.save!
+    end
+  rescue ActiveRecord::ActiveRecordError => e
+    Bugsnag.notify("Referral Error\nReferrer: #{@referrer.full_name}\n" \
+                   "User ID: #{@referrer.id}\nError: #{e}")
+    false
+  end
+end
