c450b36fcfd30d53538b4678733258e76ece6a11
James Hong <ojameso@hotmail.com>
2018-04-02 10:08:52 -0700

    Removes migration testing routes

--- config/routes/www.rb
+++ config/routes/www.rb
@@ -1,23 +1,3 @@
-class TestTopTutoring
-  def self.matches?(request)
-    # Do not prepend "www" for now to
-    # keep the domain as short as possible
-    sub = request.subdomain
-    splitter_leader(sub) == "test"
-  end
-end
-
-Rails.application.routes.draw do
-  constraints TestTopTutoring do
-    get "/sign_in" => "sessions#new", as: "test_login"
-    constraints Clearance::Constraints::SignedIn.new { |user| user.has_role?("admin") || user.has_role?("director") } do
-      get "/test_sign_out" => "sessions#destroy"
-      get "/*path" => "pages#show"
-      root to: "pages#home"
-    end
-  end
-end
-
 class WwwTopTutoring
   def self.matches?(request)
     # Do not prepend "www" for now to
