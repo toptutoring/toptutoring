b91a9d53556f3cfdecf27a625d543ffad67d4691
James Hong <ojameso@hotmail.com>
2018-04-06 09:08:13 -0700

    Removes unnecessary worker

--- config/clock.rb
+++ config/clock.rb
@@ -1,10 +1,4 @@
-require 'clockwork'
-require './config/boot'
-require './config/environment'
-require 'rake'
+require "clockwork"
+require "rake"
 
-module Clockwork
-  every(2.weeks, 'update_tokens') do
-    UpdateTokensWorker.perform_async
-  end
-end
+include Clockwork
