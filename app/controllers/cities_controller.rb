cf813a1799597a42d45b75e202712ce3032533b0
Abraham Choe <abraham.choe@gmail.com>
2018-04-18 14:08:55 -0700

    Remove slug method from Cities Controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,11 +1,7 @@
 class CitiesController < ApplicationController
   def show
-    @city = City.find_by!(slug: slug)
+    @city = City.find_by!(slug: params.require(:slug))
     @phone_number = Phonelib.parse(@city.phone_number, @city.country.code)
     render layout: "authentication"
-  end
-
-  def slug
-    params.require(:slug)
   end
 end
