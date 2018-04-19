dfe6137a32a1a4b9b386704aaba0072a86906db5
James Hong <ojameso@hotmail.com>
2018-04-19 15:17:02 -0700

    Creates Blog Categories index
    From the index, you are able to create a new blog category, update the name, and remove a category. Total post counts are also shown.

--- app/controllers/admin/blogs/blog_categories_controller.rb
+++ app/controllers/admin/blogs/blog_categories_controller.rb
@@ -1,14 +1,21 @@
 class Admin::Blogs::BlogCategoriesController < ApplicationController
-  # for now, only for admin/director/contractor
   before_action :require_login
-  before_action :set_post, only: [:show, :edit]
-  layout "authentication"
 
   def index
-    @categories = BlogCategory.all
+    @categories = BlogCategory.all.order("LOWER(name)")
   end
 
   def create
+    @category = BlogCategory.new(name: category_params)
+    if @category.save
+      flash.notice = "#{@category.name} has been added."
+    else
+      flash.alert = "#{@category.name} could not be added."
+    end
+    redirect_to action: :index
+  end
+
+  def form_create
     @category = BlogCategory.new(name: category_params)
     if @category.save
       flash.now[:notice] = "#{@category.name} has been added."
@@ -18,6 +25,18 @@ class Admin::Blogs::BlogCategoriesController < ApplicationController
   end
 
   def update
+    @category = BlogCategory.find(params[:id])
+    @updated = @category.update(name: category_params)
+    flash.now[:alert] = @category.errors.full_messages unless @updated
+  end
+
+  def destroy
+    @category = BlogCategory.find(params[:id])
+    if @category.destroy
+      flash.now[:notice] = "#{@category.name} has been removed."
+    else
+      flash.now[:alert] = "#{@category.name} could not be added."
+    end
   end
 
   private
