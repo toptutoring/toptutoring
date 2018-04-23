180bbfb61c930c77916e7372a5c3c688b04457e2
James Hong <ojameso@hotmail.com>
2018-04-23 12:50:23 -0700

    Adds zip/address to city form

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ApplicationRecord
   belongs_to :country
 
   validates :phone_number, phone: { possible: true, country_specifier: -> city { city.country.code } }
-  validates_presence_of :name, :description, :country_id, :phone_number
+  validates_presence_of :name, :description, :country_id, :phone_number, :state, :zip, :address
   validates :slug, uniqueness: true
 
   def add_slug
