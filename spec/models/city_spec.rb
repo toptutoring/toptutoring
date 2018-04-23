c2655f8838bd4befa805abbf205bfd2c5f7a8a00
Abraham Choe <abraham.choe@gmail.com>
2018-04-23 10:40:44 -0700

    Add uniqueness validation to City specs
    Also fixes the add slug test to see if a city's slug is set instead of testing the method's return value.

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper'
 
 RSpec.describe City, type: :model do
   let(:country) { Struct.new(:code).new('us') }
+  subject { FactoryBot.create(:city, name: 'Los Angeles', state: 'CA') }
   before { allow(subject).to receive(:country).and_return(country) }
 
-  it { should belong_to(:country) }
+  describe "validations" do
+    it { should belong_to(:country) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of (:country_id) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_uniqueness_of(:slug).ignoring_case_sensitivity }
+  end
 
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of (:country_id) }
-  it { should validate_presence_of(:phone_number) }
-
-  let(:city) { FactoryBot.create(:city, name: 'Los Angeles', state: 'CA') }
   describe "#add_slug" do
     it "adds a slug consisting of the city name and state" do
-      expect(city.add_slug).to eq("los-angeles-ca-tutoring")
+      subject.add_slug
+      expect(subject.slug).to eq("los-angeles-ca-tutoring")
     end
   end
 end
