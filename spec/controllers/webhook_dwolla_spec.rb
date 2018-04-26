1612484f5e7ced048869810a3c75bd910e371298
James Hong <ojameso@hotmail.com>
2018-04-26 16:32:37 -0700

    Updates webhook controller/service specs

--- spec/controllers/webhook_dwolla_spec.rb
+++ spec/controllers/webhook_dwolla_spec.rb
@@ -8,7 +8,7 @@ describe Webhooks::DwollaController do
     end 
 
     it "skips action if already processed" do
-      event_id = "event_id"
+      event_id = "an_id"
       FactoryBot.create(:dwolla_event, event_id: event_id)
 
       post :update, params: { id: event_id, _links: { resource: { href: "url" } }, topic: "transfer_failed" }
@@ -17,32 +17,29 @@ describe Webhooks::DwollaController do
     end
 
     it "processes failed transfer if not already processed" do
-      transfer_url = "transfer_url"
       dwolla_service = class_double("DwollaCompleteTransferService").as_stubbed_const
-      expect(dwolla_service).to(receive(:perform!)).with(transfer_url, "failed")
+      expect(dwolla_service).to(receive(:perform!)).and_return(true)
 
       expect {
-        post :update, params: { id: "event_id", _links: { resource: { href: transfer_url } }, topic: "transfer_failed" }
+        post :update, params: { id: "event_id", _links: { resource: { href: "url" } }, topic: "transfer_failed" }
       }.to change(DwollaEvent, :count).by 1
     end
 
     it "processes completed transfer if not already processed" do
-      transfer_url = "transfer_url"
-      dwolla_service= class_double("DwollaCompleteTransferService").as_stubbed_const
-      expect(dwolla_service).to(receive(:perform!)).with(transfer_url, "paid")
+      dwolla_service = class_double("DwollaCompleteTransferService").as_stubbed_const
+      expect(dwolla_service).to(receive(:perform!)).and_return(true)
 
       expect {
-        post :update, params: { id: "event_id", _links: { resource: { href: transfer_url } }, topic: "transfer_completed" }
+        post :update, params: { id: "event_id", _links: { resource: { href: "url" } }, topic: "transfer_completed" }
       }.to change(DwollaEvent, :count).by 1
     end
 
     it "processes mass pay update if not already processed" do
-      mass_pay_url = "mass_pay_url"
       dwolla_service= class_double("DwollaMassPayUpdateService").as_stubbed_const
-      expect(dwolla_service).to(receive(:perform!)).with(mass_pay_url)
+      expect(dwolla_service).to(receive(:perform!)).and_return(true)
 
       expect {
-        post :update, params: { id: "event_id", _links: { resource: { href: mass_pay_url } }, topic: "mass_payment_completed" }
+        post :update, params: { id: "event_id", _links: { resource: { href: "url" } }, topic: "mass_payment_completed" }
       }.to change(DwollaEvent, :count).by 1
     end
   end
--- spec/factories/dwolla_event.rb
+++ spec/factories/dwolla_event.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :dwolla_event do
     event_id "dwolla_event_id"
+    topic "transfer_completed"
+    resource_url "a_resource_url"
   end
 end
