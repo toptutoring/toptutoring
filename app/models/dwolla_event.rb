803bd60c4bd3f3cde85c502d139a77d22224bb4c
James Hong <ojameso@hotmail.com>
2018-04-26 16:30:19 -0700

    Associates dwolla event with payouts

--- app/models/dwolla_event.rb
+++ app/models/dwolla_event.rb
@@ -1,2 +1,15 @@
 class DwollaEvent < ApplicationRecord
+  has_one :payout, foreign_key: :dwolla_transfer_url, primary_key: :resource_url
+  has_many :mass_pay_payouts, class_name: "Payout", foreign_key: :dwolla_mass_pay_url, primary_key: :resource_url
+
+  def payout_status
+    case topic
+    when "transfer_completed"
+      "paid"
+    when "transfer_failed"
+      "failed"
+    when "transfer_cancelled"
+      "cancelled"
+    end
+  end
 end
