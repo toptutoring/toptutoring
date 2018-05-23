dbafdaa667bcacfba112ce9a0e38c37e71f0faef
James Hong <ojameso@hotmail.com>
2018-05-23 10:34:13 -0700

    Refactors payment service
    Much of the payment service actions should be the responsibility of the payment model. Therefore, the actions were moved to the payment model to slim down the service.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -21,11 +21,47 @@ class Payment < ActiveRecord::Base
   scope :succeeded, -> { where(status: "succeeded") }
   scope :refunded, -> { where(status: "refunded") }
 
+  def set_rate
+    return if one_time
+    self.rate_cents = case hours_type
+                      when "online_academic"
+                        payer.online_academic_rate_cents
+                      when "online_test_prep"
+                        payer.online_test_prep_rate_cents
+                      when "in_person_academic"
+                        payer.in_person_academic_rate_cents
+                      when "in_person_test_prep"
+                        payer.in_person_test_prep_rate_cents
+                      end
+  end
+
+  def set_amount
+    return if one_time
+    self.amount_cents = (hours_purchased * rate_cents).round
+  end
+
+  def set_default_description
+    return if one_time
+    hours = hours_purchased
+    type = hours_type.humanize
+    self.description = "Purchase of #{hours} #{type} hours."
+  end
+
+
   def card_brand_and_four_digits
     card_brand + " ending in ..." + last_four
   end
 
   def payer_name
     payer ? payer.full_name : card_holder_name
+  end
+
+  def fully_refunded?
+    hours_purchased == refunds.sum(:hours)
+  end
+
+  def refundable_hours
+    return nil if one_time
+    hours_purchased - refunds.sum(:hours)
   end
 end
