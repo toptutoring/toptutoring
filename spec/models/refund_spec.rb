56f325db9ec52684bab67010d5e6b9fd44425ca6
James Hong <ojameso@hotmail.com>
2018-05-23 13:13:24 -0700

    Add specs for Refund and Refund Service

---
+++ spec/models/refund_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Refund, type: :model do
+  context "validations" do
+    it { should belong_to(:payment) }
+    it { should validate_presence_of(:payment) }
+    it { should validate_presence_of(:stripe_refund_id) }
+    it { should validate_presence_of(:reason) }
+    it { should_not allow_value(0).for(:amount) }
+  end
+
+  describe ".set_amount" do
+    it "sets amount from hours and payment rate" do
+      payment = FactoryBot.create(:payment, :hourly_purchase, rate_cents: 25_00)
+      refund = FactoryBot.build(:refund, hours: 1, amount_cents: nil, payment: payment)
+      expect(refund.amount_cents).to eq nil
+      refund.set_amount
+      expect(refund.amount_cents).to eq 25_00
+    end
+
+    it "sets amount from hours and payment rate" do
+      payment = FactoryBot.create(:payment, :hourly_purchase, rate_cents: 30_00)
+      refund = FactoryBot.build(:refund, hours: 2, amount_cents: nil, payment: payment)
+      expect(refund.amount_cents).to eq nil
+      refund.set_amount
+      expect(refund.amount_cents).to eq 60_00
+    end
+  end
+end
