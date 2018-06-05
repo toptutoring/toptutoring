169b1d3a7496022fcc2f83435172260f44305191
James Hong <ojameso@hotmail.com>
2018-06-05 09:45:54 -0700

    Adds destroy action for admin on reviews

--- app/controllers/admin/client_reviews_controller.rb
+++ app/controllers/admin/client_reviews_controller.rb
@@ -6,10 +6,19 @@ module Admin
     end
 
     def create
-      client = User.find_by_id(params[:client_id])
-      UserNotifierMailer.send_review_request(client)
-      flash.notice = "Review has been requested."
+      client = User.find_by_id(params[:user_id])
+      UserNotifierMailer.send_review_request(client).deliver_later
+      flash.notice = "Review for #{client.full_name} has been requested."
       redirect_to admin_client_reviews_path
+    end
+
+    def destroy
+      @review = ClientReview.find(params[:id])
+      if @review.destroy
+        flash.now.notice = "Review has been removed."
+      else
+        flash.now.alert = "Review could not be removed."
+      end
     end
   end
 end
