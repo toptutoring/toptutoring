79efea023c61a24de048e638fdbceca29cd4bb45
James Hong <ojameso@hotmail.com>
2018-06-06 11:18:36 -0700

    Updates and adds specs for client_account/review

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -1,36 +1,61 @@
 require "rails_helper"
 
+def make_purchase(client)
+  sign_in client
+  visit new_clients_payment_path
+
+  allow_any_instance_of(Clients::PaymentsController).to receive(:token_id) { "tok_visa" }
+  allow_any_instance_of(ClientAccount).to receive(:request_review?) { true }
+
+  fill_in "payment_hours_purchased", with: 2
+  fill_in "card_holder_name", with: client.full_name
+  fill_in "card_address", with: "413 Willow"
+  fill_in "card_city", with: "Alameda"
+  fill_in "card_zip_code", with: "94501"
+  click_on "Purchase Hours"
+end
+
 feature "Add a review" do
   let(:client) { FactoryBot.create(:client_user) }
+  let!(:engagement) { FactoryBot.create(:engagement, client_account: client.client_account) }
 
   scenario "when client gives a 5 star review", js: true do
-    visit new_review_path(client.unique_token)
-    expect(page).to have_content("Hello #{client.full_name}. Let us know how we're doing!")
+    make_purchase(client)
+    expect(page).to have_content I18n.t("app.clients.reviews.request_rating", name: client.full_name)
     expect(page).to have_content("Rating")
-    expect(page).to have_content("Review")
 
-    review_text = "This is a review"
-
-    fill_in "client_review_review", with: review_text
     find("label[for=client_review_stars_5]").click
-    click_on "Submit Review"
+    click_on "Submit Rating"
 
-    expect(page).to have_content "Thank you for your review!"
-    expect(page).to have_link "Add a review to Google"
-    expect(page).to have_content review_text
+    expect(page).to have_content I18n.t("app.clients.reviews.request_external_review_html", href: ENV.fetch("DEFAULT_REVIEW_SOURCE"))
+    expect(page).to have_link href: ENV.fetch("DEFAULT_REVIEW_LINK")
+  end
+
+  scenario "when client gives a 5 star review and has a location/link set", js: true do
+    link = "https://www.toptutoring.com"
+    location = "Top Tutoring"
+    client.client_account.update(review_link: link, review_source: location)
+    make_purchase(client)
+    expect(page).to have_content I18n.t("app.clients.reviews.request_rating", name: client.full_name)
+    expect(page).to have_content("Rating")
+
+    find("label[for=client_review_stars_5]").click
+    click_on "Submit Rating"
+
+    expect(page).to have_content I18n.t("app.clients.reviews.request_external_review_html", href: location)
+    expect(page).to have_link href: link
   end
 
   scenario "when client gives less than a 5 star review", js: true do
-    visit new_review_path(client.unique_token)
+    make_purchase(client)
+    find("label[for=client_review_stars_4]").click
+    click_on "Submit Rating"
 
     review_text = "This is a review"
-
     fill_in "client_review_review", with: review_text
-    find("label[for=client_review_stars_4]").click
     click_on "Submit Review"
 
-    expect(page).to have_content "Thank you for your review!"
-    expect(page).not_to have_link "Add a review to Google"
+    expect(page).to have_content I18n.t("app.clients.reviews.thank_you")
     expect(page).to have_content review_text
   end
 end
--- spec/models/client_account_spec.rb
+++ spec/models/client_account_spec.rb
@@ -2,8 +2,35 @@ require 'rails_helper'
 
 RSpec.describe ClientAccount, type: :model do
   let(:client) { FactoryBot.create(:client_user, online_test_prep_rate: 0, in_person_test_prep_rate: 0, in_person_academic_rate: 0) }
+  let(:engagement) { FactoryBot.create(:engagement, client_account: client.client_account) }
 
-  it { should validate_presence_of(:user) }
+  context "Validations" do
+    it { should validate_presence_of(:user) }
+
+    it "validates presence of review link if review location is present" do
+      subject = client.client_account
+      subject.assign_attributes(review_source: "location")
+      expect(subject.valid?).to be false
+    end
+
+    it "validates presence of review location if review link is present" do
+      subject = client.client_account
+      subject.assign_attributes(review_link: "https://www.toptutoring.com")
+      expect(subject.valid?).to be false
+    end
+
+    it "should be valid if both location and link is provided" do
+      subject = client.client_account
+      subject.assign_attributes(review_source: "location", review_link: "https://www.toptutoring.com")
+      expect(subject.valid?).to be true
+    end
+
+    it "validates review link to be valid url" do
+      subject = client.client_account
+      subject.assign_attributes(review_source: "location", review_link: "not url")
+      expect(subject.valid?).to be false
+    end
+  end
 
   describe "#academic_types_engaged" do
     it "returns only the types engaged where rate is greater than 0" do
@@ -11,36 +38,31 @@ RSpec.describe ClientAccount, type: :model do
     end
   end
 
-  describe "#send_review_email" do
-    let(:engagement) { FactoryBot.create(:engagement, client_account: client.client_account) }
-
-    it "returns true if review should be requested" do
-      3.times do 
-        FactoryBot.create(:invoice, session_rating: 5, engagement: engagement, submitter: engagement.tutor_account.user)
-      end
-      expect(client.client_account.send_review_email?).to be_truthy
+  describe "#request_review?" do
+    it "returns false if client already has a review" do
+      FactoryBot.create(:client_review, client_account: client.client_account)
+      expect(client.client_account.request_review?).to eq false
     end
 
-    it "returns false if review has already been requested" do
-      3.times do 
-        FactoryBot.create(:invoice, session_rating: 5, engagement: engagement, submitter: engagement.tutor_account.user)
-      end
-      client.client_account.update(review_requested: true)
-      expect(client.client_account.send_review_email?).to be_falsey
+    it "returns false if client has no invoices" do
+      expect(client.client_account.request_review?).to eq false
     end
 
-    it "returns false if client has less than 3 invoices" do
-      2.times do 
-        FactoryBot.create(:invoice, session_rating: 5, engagement: engagement, submitter: engagement.tutor_account.user)
+    it "returns false if client has many invoices but they are less than 4 stars" do
+      3.times do
+        FactoryBot.create(:invoice, client: client, session_rating: 3, engagement: engagement)
       end
-      expect(client.client_account.send_review_email?).to be_falsey
+
+      expect(client.client_account.request_review?).to eq false
     end
 
-    it "returns false if client has no 5 star session ratings" do
-      3.times do 
-        FactoryBot.create(:invoice, session_rating: 4, engagement: engagement, submitter: engagement.tutor_account.user)
+    it "returns true if client has two invoices and one is at least 4 stars" do
+      2.times do
+        FactoryBot.create(:invoice, client: client, session_rating: 3, engagement: engagement)
       end
-      expect(client.client_account.send_review_email?).to be_falsey
+      client.client_account.invoices.last.update(session_rating: 4)
+
+      expect(client.client_account.request_review?).to eq true
     end
   end
 end
