c2e46079a62e3d62ca77aaeeb3a4a773599c8c4d
James Hong <ojameso@hotmail.com>
2018-06-06 10:51:35 -0700

    Link to external review source dynamically

--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -2,4 +2,18 @@ module ReviewHelper
   def review_stars(number)
     tag.span "★" * number, class: "stars"
   end
+
+  def review_external_link(user)
+    account = user.client_account
+    link_to client_review_source(account),
+      client_review_link(account), target: :blank
+  end
+
+  def client_review_source(account)
+    account.review_source || ENV.fetch("DEFAULT_REVIEW_SOURCE")
+  end
+
+  def client_review_link(account)
+    account.review_link || ENV.fetch("DEFAULT_REVIEW_LINK")
+  end
 end
