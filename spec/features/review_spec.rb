b98c4d91c8854d8276361e8ed7946506242484ef
James Hong <ojameso@hotmail.com>
2018-06-08 10:41:21 -0700

    Show request for review in modals

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -9,9 +9,9 @@ def make_purchase(client)
 
   fill_in "payment_hours_purchased", with: 2
   fill_in "card_holder_name", with: client.full_name
-  fill_in "card_address", with: "413 Willow"
-  fill_in "card_city", with: "Alameda"
-  fill_in "card_zip_code", with: "94501"
+  fill_in "card_address", with: "1234 Address"
+  fill_in "card_city", with: "City"
+  fill_in "card_zip_code", with: "01234"
   click_on "Purchase Hours"
 end
 
@@ -21,8 +21,8 @@ feature "Add a review" do
 
   scenario "when client gives a 5 star review", js: true do
     make_purchase(client)
+    expect(page).to have_content(I18n.t("app.clients.reviews.heading"))
     expect(page).to have_content I18n.t("app.clients.reviews.request_rating", name: client.full_name)
-    expect(page).to have_content("Rating")
 
     find("label[for=client_review_stars_5]").click
     click_on "Submit Rating"
@@ -33,11 +33,11 @@ feature "Add a review" do
 
   scenario "when client gives a 5 star review and has a location/link set", js: true do
     link = "https://www.example.com"
-    location = "Top Tutoring"
+    location = "Example Review Site"
     client.client_account.update(review_link: link, review_source: location)
     make_purchase(client)
+    expect(page).to have_content(I18n.t("app.clients.reviews.heading"))
     expect(page).to have_content I18n.t("app.clients.reviews.request_rating", name: client.full_name)
-    expect(page).to have_content("Rating")
 
     find("label[for=client_review_stars_5]").click
     click_on "Submit Rating"
