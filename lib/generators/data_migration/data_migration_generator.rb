fc75644bafd65ddec8432fa0814935a93f21f546
Tad Park <tadp@users.noreply.github.com>
2018-08-23 22:48:22 -0700

    Make generator specific to Rails migrations

--- lib/generators/data_migration/data_migration_generator.rb
+++ lib/generators/data_migration/data_migration_generator.rb
@@ -1,3 +1,9 @@
-class DataMigrationGenerator < Rails::Generators::NamedBase
+require 'rails/generators/active_record'
+
+class DataMigrationGenerator < ActiveRecord::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+
+  def create_migration_file
+    migration_template "migration.rb", "db/migrate/#{file_name}.rb"
+  end
 end
---
+++ lib/generators/data_migration/templates/migration.rb
@@ -0,0 +1,4 @@
+class <%= migration_class_name %> < DataMigration
+  UP_RAKE_TASK = place_rake_task_as_string_here
+  # DOWN_RAKE_TASK = "fill this in or delete me if unnecessary"
+end
