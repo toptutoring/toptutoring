ffa671865fe8b62cae522194a2c503f3005f49aa
Tad Park <tadp@users.noreply.github.com>
2018-10-30 05:55:59 -0700

    Extract stripe link into a helper so that tutor directors and tutors can authenticate

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -18,13 +18,6 @@ class DashboardsController < ApplicationController
   def tutor
     @tutor_engagements = current_user.tutor_account.engagements.active.includes(:client_account, :subject, :student_account)
     @pending_invoices = current_user.invoices.by_tutor.pending.includes(:engagement)
-    base_url = "https://connect.stripe.com/express/oauth/authorize?&client_id="
-    csrf_hash = "&state=#{SecureRandom.hex(8)}"
-    business_type = "&stripe_user[business_type]=individual"
-    first_name = "&stripe_user[first_name]=#{current_user.first_name}"
-    last_name = "&stripe_user[last_name]=#{current_user.last_name}"
-    email = "&stripe_user[email]=#{current_user.email}"
-    @stripe_url = URI.encode(base_url + ENV.fetch("STRIPE_CONNECT_CLIENT_ID") + csrf_hash + business_type + first_name + last_name + email)
     @invoice = Invoice.new
   end
 
--- app/helpers/payment_helper.rb
+++ app/helpers/payment_helper.rb
@@ -19,6 +19,16 @@ module PaymentHelper
     tag.i class: "icon #{icon_type}"
   end
 
+  def stripe_connect_auth_link
+    base_url = "https://connect.stripe.com/express/oauth/authorize?&client_id="
+    csrf_hash = "&state=#{SecureRandom.hex(8)}"
+    business_type = "&stripe_user[business_type]=individual"
+    first_name = "&stripe_user[first_name]=#{current_user.first_name}"
+    last_name = "&stripe_user[last_name]=#{current_user.last_name}"
+    email = "&stripe_user[email]=#{current_user.email}"
+    URI.encode(base_url + ENV.fetch("STRIPE_CONNECT_CLIENT_ID") + csrf_hash + business_type + first_name + last_name + email)
+  end
+
   def payment_amount_display_text(payment)
     if payment.class == Refund
       "(#{number_to_currency(payment.amount)})"
